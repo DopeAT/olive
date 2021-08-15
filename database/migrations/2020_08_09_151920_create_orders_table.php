f293fe6d17c91b3c11c2fafe132a713095c6bc6e
Alex Tser <alextser@yahoo.co.uk>
2021-08-15 16:06:30 +0100

    Order - Product Migration

---
+++ database/migrations/2020_08_09_151920_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('payment_id');
+            $table->string('order_full_name');
+            $table->string('order_email');
+            $table->string('order_phone');
+            $table->text('order_address');
+            $table->unsignedBigInteger('product_id');
+            $table->enum('status', ['completed', 'in-progress', 'cancelled', 'failed']);
+            $table->boolean('is_new')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
